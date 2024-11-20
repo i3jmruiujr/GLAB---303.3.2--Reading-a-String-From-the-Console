34d8ab9e60e803a429130d59295ffa1bf24d5ffa
Rana Ismael <hassnhassoon4@gmail.com>
2024-11-20 11:53:07 -0500

    GLAB - 303.3.2 -Reading a String From the Console

---
+++ src/ReadingStringFromConsole.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ReadingStringFromConsole {
+    public void readingStringFrom() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        String s = input.nextLine();
+        char ch = s.charAt(0);
+        System.out.println("The character entered is " + ch);
+    }
+}
